718f28d65688fd57a89bf1cee85d2e08dae98181
ido4848 <ido.aizenbud@gmail.com>
2016-09-10 13:37:44 +0300

    updated project index/init structure

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-var indexModel = require('./src/indexModel');
-var search = require('./src/search');
+var init = require('./src/init');
 
-module.exports = {
-  indexModel: indexModel,
-  search: search
-};
+var searchEngine = {
+  initialized: false,
+  init: function(options) {
+    options = options ? options : {};
+    try {
+      require('./searchTagModel');
+    } catch(e) {
+      return Promise.reject(e);
+    }
+    this.initialized = true;
+    if (!options.quiet) {
+      console.log("Tag-based search engine was initiated.");
+    }
+    return Promise.resolve();
+  },
+  get indexModel(){
+    if(!initialized){
+      throw new Error("Search engine was initialized!");
+    }
+    return require('./src/indexModel');
+  }
+
+  get search(){
+    if(!initialized){
+      throw new Error("Search engine was initialized!");
+    }
+    return require('./src/search');
+  }
+
+}
+
+module.exports = searchEngine;
--- src/init.js
+++
@@ -1,8 +0,0 @@
-module.exports = function init(options){
-  options = options ? options : {};
-  require('./searchTagModel');
-  if (!options.quiet) {
-    console.log("Tag-based search engine was initiated.");
-  }
-  return Promise.resolve();
-}
